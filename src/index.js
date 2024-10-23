354b697b72251296fd32dcb81c39d5b098aca161
gunal-01 <gunalprakash006@gmail.com>
2024-10-23 22:32:16 +0530

    practiced(10/23/24)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Prac from './prac';
-import ImplementingCss from './implementingCss';
+import ImplementingCss from './ImplementingCssInReact/implementingCss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
